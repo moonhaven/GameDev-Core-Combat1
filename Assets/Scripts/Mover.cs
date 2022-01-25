faf319fdaab5300e76c2519c632d52e989089492
Tony Sims <tonysims222@gmail.com>
2022-01-25 15:08:48 -0500

    Initial Commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,29 @@
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+
+ 
+        
+}
